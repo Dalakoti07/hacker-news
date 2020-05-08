8cecee9fd97c399669cc252ce16ecb6e7750be77
dalakoti07 <saurabhdalakoti07@gmail.com>
2020-05-08 16:19:59 +0530

    Updates

---
+++ precache-manifest.9dccd436da22f2af4a39df13b4557072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2f2bcb4cfbc54cfe1cab9ad817a3c9",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "9e2b96ae22bc13db071c",
+    "url": "/hacker-news/static/css/main.bf9f74ea.chunk.css"
+  },
+  {
+    "revision": "79ee1ea93578e0544c88",
+    "url": "/hacker-news/static/js/2.3f2ae525.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.3f2ae525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e2b96ae22bc13db071c",
+    "url": "/hacker-news/static/js/main.9c82e08e.chunk.js"
+  },
+  {
+    "revision": "4ac271de384704436b1f",
+    "url": "/hacker-news/static/js/runtime-main.a4bc4e50.js"
+  }
+]);
